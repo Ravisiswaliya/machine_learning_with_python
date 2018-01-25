2ccbebe9da52f63922d9befd8ea710a6c46606b2
ravisiswaliya <ravisiswaliya@gmail.com>
2018-01-25 08:36:27 +0530

    Simple Linear Regression

--- data_preprocessing/data_preprocess.py
+++ data_preprocessing/data_preprocess.py
@@ -1,37 +1,26 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+from sklearn.preprocessing import Imputer,OneHotEncoder,LabelEncoder
 
-#read data set
-
+#loading dataset
 dataset = pd.read_csv('city.csv')
-#print(dataset)
 
-X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,3].values
+X = dataset.iloc[:,:-1].values  # 0 to 3 column as x
 #print(X)
+y = dataset.iloc[:,3].values   # selecting last column as y
 #print(y)
 
-#handling missing values
-'''
-here we are converting all nan value 
-imputer will fill all NaN value into total mean average value
-'''
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN',strategy="mean",axis=0)
-imputer = imputer.fit(X[:,1:3])
-X[:,1:3] = imputer.fit_transform(X[:,1:3])
-#print(X)
 
-'''
-here we are using lanbel encoder and one hot encoder 
-that will convert all string value into numric values
-'''
+#handling missing value
+imputer = Imputer(missing_values="NaN",strategy='mean',axis=0)
+X[:,1:3] = imputer.fit_transform(X[:,1:3])
+
+#labeling
 labelencoder = LabelEncoder()
 X[:,0] = labelencoder.fit_transform(X[:,0])
-#print(X)
+
+y = labelencoder.fit_transform(y)
 
 onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
-#print(X)
+print(X)
 
-y = labelencoder.fit_transform(y)
